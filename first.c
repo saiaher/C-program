9d34b6a1dd6b7661a082f513a03c17efe1677255
Omkar Aher <aher.omkar.15et7021@gmail.com>
2025-09-25 16:34:45 +0530

    c sprint

---
+++ first.c
@@ -0,0 +1,20 @@
+
+// stander code 
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n"); 
+    return 0; 
+
+
+    int num;
+    //// i/o operator
+
+    printf("Enter a number: ");      
+    scanf("%d", &num);              
+
+    printf("You entered: %d\n", num);  
+
+    return 0;
+}
+
