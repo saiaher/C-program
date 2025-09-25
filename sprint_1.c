5b46bb31c6e9a49007eb01e5e6d2351eca66a0bc
Omkar Aher <aher.omkar.15et7021@gmail.com>
2025-09-25 16:42:42 +0530

    c type conversion

---
+++ sprint_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// type conversion 
+
+// in this code after sum a is convert into sum
+
+
+int main() {
+    int a = 5;       
+    float b = 3.2;   
+    float sum;
+
+    sum = a + b;  
+    printf("sum: %.2f\n", sum);  
+    return 0;
+}
