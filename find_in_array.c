e0daac8cef267b6ed32cc24865af023a9a778335
Omkar Aher <aher.omkar.15et7021@gmail.com>
2025-12-23 20:59:08 +0530

    find smallest

--- find_in_array.c
+++ find_in_array.c
@@ -4,7 +4,7 @@ int main(){
     int arr[]={9,22,34,343,23,2,23,432,1,3};
     int min = arr[0];
     for(int i=1;i<=10;i++){
-        if(arr[i] > min){
+        if(arr[i] < min){
             min = arr[i];
 
         }
